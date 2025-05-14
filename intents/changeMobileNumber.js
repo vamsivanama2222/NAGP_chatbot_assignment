7596e083ab10ae0c8cb4b3b68a632182a292e5b5
Vamsi Vanama <vamsi.vanama@nagarro.com>
2025-05-14 23:45:55 +0530

    Adding Code

--- intents/changeMobileNumber.js
+++ intents/changeMobileNumber.js
@@ -1,6 +1,6 @@
 module.exports = function changeMobileNumber(agent) {
     agent.context.set({ name: 'got_mobile', lifespan: 0 });
     agent.context.set({ name: 'ask_mobile_number', lifespan: 2 });
-    agent.add("Sure, please provide your new mobile number.");
+    agent.add("Sure, please Share your Number.");
   };
   
--- intents/welcome.js
+++ intents/welcome.js
@@ -1,4 +1,4 @@
 module.exports = function welcome(agent) {
-    agent.add("Hi, welcome to ABC Mutual Fund Services. What service would you like to use?");
-    agent.add("Quick Suggestions:\n- Portfolio Valuation\n- Explore Funds\n- Transaction History");
+    agent.add("Hi, welcome to CARING Mutual Fund Services. What service would you like to use?");
+    agent.add("Here are the Quick Suggestions:\n- Portfolio Valuation\n- Explore Funds\n- Transaction History");
   };
